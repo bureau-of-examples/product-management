d7e143589f645ef9da2fc4a889e5b358da41f9f8
zhy2002 <hz141@uowmail.edu.au>
2015-10-05 10:33:30 +1100

    implementing the search tags form

--- src/main/webapp/app/products/productEditCtrl.js
+++ src/main/webapp/app/products/productEditCtrl.js
@@ -24,14 +24,31 @@
         };
 
         vm.submit = function() {
-            productResource.save(vm.product);
+            productResource.save(vm.product).$promise.then(function(){
+                toastr.success("Save successful.");
+            });
         };
 
         vm.cancel = function() {
             $state.go("productList");
-        }
-    }
+        };
 
+        vm.addTags = function(tags){
+            if(tags){
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                vm.newTags = "";
+            } else {
+                alert("Please enter one or more tags separated by commas.");
+            }
+        };
+
+        vm.removeTag = function(idx){
+            vm.product.tags.splice(idx, 1);
+        };
+
+
+    }
 
 
 }());
