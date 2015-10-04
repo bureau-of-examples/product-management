d0b92ca062200bf59968b7e6e15c1137de8cc615
zhy2002 <hz141@uowmail.edu.au>
2015-10-04 17:45:28 +1100

    building the nested templates

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -19,25 +19,41 @@
             controller: "productListCtrl as vm"
         });
 
+        var resolveProductById = {
+            productResource:"productResource",
+
+            product:["productResource", "$stateParams", function(productResource, $stateParams){
+                var productId = $stateParams.productId;
+                return productResource.get({productId:productId}).$promise;
+            }]
+
+        };
+
         $stateProvider.state("productEdit", {
+            abstract: true,
             url: "/products/edit/:productId",
             templateUrl:"app/products/productEditView.html",
-            controller: "productEditCtrl as vm"
-        });
+            controller: "productEditCtrl as vm",
+            resolve: resolveProductById
+        })
+            .state("productEdit.info", {
+               url:"/info",
+                templateUrl:"app/products/productEditInfoView.html"
+            })
+            .state("productEdit.price", {
+                url:"/price",
+                templateUrl:"app/products/productEditPriceView.html"
+            })
+            .state("productEdit.tags", {
+                url:"tags",
+                templateUrl:"app/products/productEditTagsView.html"
+            });
 
         $stateProvider.state("productDetail", {
             url: "/products/:productId",
             templateUrl:"app/products/productDetailView.html",
             controller: "productDetailCtrl as vm",
-            resolve:{
-                productResource:"productResource",
-
-                product:["productResource", "$stateParams", function(productResource, $stateParams){
-                    var productId = $stateParams.productId;
-                    return productResource.get({productId:productId}).$promise;
-                }]
-
-            }
+            resolve: resolveProductById
         });
     }
 
---
+++ src/main/webapp/app/products/productEditCtrl.js
@@ -0,0 +1,19 @@
+(function(){
+    "use strict";
+
+    angular.module("productManagement").controller("productEditCtrl", ["product", productEditCtrl]);
+
+    function productEditCtrl(product){
+        var vm = this;
+
+        vm.product = product;
+
+        if(vm.product && vm.product.productId){
+            vm.title = "Edit: " + vm.product.productName;
+        } else {
+            vm.title = "New Product";
+        }
+    }
+
+}());
+
