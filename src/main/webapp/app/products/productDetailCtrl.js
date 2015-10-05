1aec51f56ceda420a8da18ff667f0e11e24df3de
zhy2002 <hz141@uowmail.edu.au>
2015-10-05 11:50:25 +1100

    using a custom angular service

--- src/main/webapp/app/products/productDetailCtrl.js
+++ src/main/webapp/app/products/productDetailCtrl.js
@@ -3,15 +3,17 @@
 
     angular
         .module("productManagement")
-        .controller("productDetailCtrl", ["product", productDetailCtrl]);
+        .controller("productDetailCtrl", ["product", "productService", productDetailCtrl]);
 
-    function productDetailCtrl(product){
+    function productDetailCtrl(product, productService){
 
         var vm = this;
 
         vm.product = product;
 
         vm.title = "Product Detail: " + vm.product.productName;
+
+        vm.marginPercent = productService.calculateMarginPercent(product.price, product.cost);
 
         if(vm.product.tags){
             vm.product.tagList = vm.product.tags.toString();
